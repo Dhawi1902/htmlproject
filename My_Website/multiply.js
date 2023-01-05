297b224ecd4566c13fc8a12599593916723a9106
dhawi1902 <dhawi.ars02@gmail.com>
2023-01-05 09:07:29 +0800

    new

--- My_Website/multiply.js
+++
@@ -1,7 +0,0 @@
-
-
-  function multiply(){
-      var number = document.getElementById("num").value;
-
-      document.getElementById("result").innerHTML = number * 2;
-}
--- My_Website/product.js
+++
@@ -1,8 +0,0 @@
-
-  function product(a , b){
-      var num1 = Number(a);
-      var num2 = Number(b);
-      let result = num1 + num2;
-
-      document.getElementById("total").innerHTML = result;
-}
