778b90f189b64be9d069624eabd28db0e644fea7
LipJiq <alif.razmi@live.iium.edu.my>
2023-01-09 21:46:28 +0800

    kk

--- Gp_Project/form_validation.js
+++ Gp_Project/form_validation.js
@@ -1,3 +1,9 @@
-function ValidateEmail(input){
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_'{}]
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    document.getElementById("error" ).innerHTML = "You have entered an invalid email address!"
+    return (false)
 }
